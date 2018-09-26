e120d1ee397e8960d9fee422426347f64a54c46d
D41JUNG0D <oonja1234@gmail.com>
2018-09-26 17:33:22 +0900

    Project Finished

--- main.py
+++ main.py
@@ -4,14 +4,26 @@ from time import sleep
 from sys import exit
 import os
 
+def art():
+    art = """   ▄▄▄▄███▄▄▄▄   ███    █▄     ▄████████    ▄████████ 
+ ▄██▀▀▀███▀▀▀██▄ ███    ███   ███    ███   ███    ███ 
+ ███   ███   ███ ███    ███   ███    █▀    ███    █▀  
+ ███   ███   ███ ███    ███   ███         ▄███▄▄▄     
+ ███   ███   ███ ███    ███ ▀███████████ ▀▀███▀▀▀     
+ ███   ███   ███ ███    ███          ███   ███    █▄  
+ ███   ███   ███ ███    ███    ▄█    ███   ███    ███ 
+  ▀█   ███   █▀  ████████▀   ▄████████▀    ██████████ 
+                                                      """
+    print(art)
+
 def chart_50():
     chart_list = chart_parse()
 
     os.system('cls')
-    print("실시간 TOP50")
+    print("실시간 TOP50\n")
     ct = 1
     for key in chart_list:
-        print(ct,". ",key," - ", chart_list[key])
+        print(str(ct)+". ",key," - ", chart_list[key])
         ct+=1
 
     print("\n아무키나 누르면 메인화면으로 돌아갑니다.")
@@ -24,7 +36,7 @@ def yt_search(search):
     print("검색어 리스트\n")
     ct = 1
     for key in yt_list:
-        print(ct,". "+key)
+        print(str(ct)+". ",key,"\n")
         ct+=1
 
     return yt_list
@@ -38,16 +50,24 @@ def yt_down(idx,yt_list):
         ct+=1
 
     print("다운중...")
-    video(yt_url)
+    if not video(yt_url):
+        print("제대로 다운되지 않았습니다.")
+        sleep(1)
+        return 0
+
     print("성공적으로 다운되었습니다.")
+    sleep(1)
+    return 1
 
 if __name__ == '__main__':
     while True:
         os.system('cls')
-        print("MUSE")
-        print("1. 실시간 차트 확인")
-        print("2. 음악 다운받기")
-        print("3. 프로그램 종료")
+        art()
+        print("\n\n\n")
+        print("Default Download Folder ./Music\n")
+        print("1. 실시간 차트 확인\n")
+        print("2. 음악 다운받기\n")
+        print("3. 프로그램 종료\n")
         try:
             ch = int(input(">> "))
         except:
